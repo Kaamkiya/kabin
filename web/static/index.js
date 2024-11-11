1bdbc647ede1ebc347df2622a65c84040fdbf76c
Kaamkiya <ziam3.1415@gmail.com>
2024-11-10 20:17:24 -0500

    feat: Improve frontend

--- web/static/index.js
+++ web/static/index.js
@@ -1,8 +1,12 @@
 const editor = ace.edit("editor");
-editor.setTheme("ace/theme/cloud_editor_dark");
-editor.session.setMode("ace/mode/golang");
+editor.setTheme("ace/theme/github_dark");
 
 const languageSelector = document.querySelector("#language-select");
+
+for (let lang of Object.keys(aceLangs)) {
+  languageSelector.innerHTML += `<option value="${aceLangs[lang].mode}">${aceLangs[lang].name}</option>`
+}
+
 languageSelector.onchange = () => {
-  editor.session.setMode("ace/mode/" + languageSelector.value);
+  editor.session.setMode(languageSelector.value);
 }
